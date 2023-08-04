7587c2cf1023f796efdac47986287f2eeb9fdd1c
lalesca_moraes <lalesca.silva@zup.com.br>
2023-08-04 15:09:23 -0300

    subindo calculateTotalArea

---
+++ src/main/kotlin/CalculateTotalArea.kt
@@ -0,0 +1,9 @@
+
+fun calculateTotalArea(form: List<Form>): Double {
+    var totalArea = 0.0
+
+    for (forms in form) {
+        totalArea += forms.calculateArea()
+    }
+    return totalArea
+}
