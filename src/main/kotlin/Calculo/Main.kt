2ebb3bae12de6a0506eefd3882134d2f59158555
Raissa Vicente <raissa.carvalho@zup.com.br>
2023-08-04 16:02:15 -0300

    Atualizando

--- src/main/kotlin/Calculo/Main.kt
+++ src/main/kotlin/Calculo/Main.kt
@@ -4,11 +4,10 @@ import Forms.Circle
 import Forms.Rectangle
 
 fun main() {
-    println("Área do retângulo: ${Rectangle(3.0, 4.0).calculateArea()}")
-    println("Área total: ${Circle(5.0).calculateArea()}")
     val formList = listOf(Circle(5.0), Rectangle(3.0, 4.0))
     val totalArea = calculos().calculateTotalArea(formList)
+    println("Área do retângulo: %.2f".format(Rectangle(3.0, 4.0).calculateArea()))
+    println("Área total: %.2f".format(Circle(5.0).calculateArea()))
     println()
     println("Área total: %.2f".format(totalArea))
-
 }
